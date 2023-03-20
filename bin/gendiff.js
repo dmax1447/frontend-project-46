1085667abd1344c4dd0a713e5bd7344d6cb73b40
Max Dragaytsev <40897499+dmax1447@users.noreply.github.com>
2023-03-20 14:21:41 +0300

    step2 add args and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,9 +4,12 @@ import {Command} from "commander";
 
 const program = new Command()
 program
-    .name('gendiff-cli')
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.1.0')
+  .name('gendiff-cli')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.1.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format' )
 program.parse();
 
 if (program.args.length) {
