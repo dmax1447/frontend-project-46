230d19caa8b22b3bd2a222e032c8717882896f2d
Max Dragaytsev <40897499+dmax1447@users.noreply.github.com>
2023-03-20 14:01:09 +0300

    upd init

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,6 @@ import {Command} from "commander";
 const program = new Command()
 program
     .name('gendiff-cli')
-    .option('h', '--help', 'display help')
     .description('Compares two configuration files and shows a difference.')
     .version('0.1.0')
 program.parse();
