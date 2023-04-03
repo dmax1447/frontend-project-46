c8b2b5c403ea9a6280a0b8f3ead7fe54c466ea70
Max Dragaytsev <40897499+dmax1447@users.noreply.github.com>
2023-04-03 10:43:31 +0300

    fp-style refactor

--- src/processors.js
+++ src/processors.js
@@ -2,13 +2,8 @@ import _ from 'lodash';
 
 function calculateDiff(obj1, obj2) {
   const merged = { ...obj1, ...obj2 };
-  const normalizedKeys = [...new Set(Object.keys(merged))]
-    .sort((el1, el2) => (el1 > el2 ? 1 : -1));
+  const normalizedKeys = _.sortBy([...new Set(Object.keys(merged))]);
   const ast = normalizedKeys.reduce((acc, key) => {
-    const type = typeof key;
-    const node = {
-      type,
-    };
     const hasKeyObj1 = Object.prototype.hasOwnProperty.call(obj1, key);
     const hasKeyObj2 = Object.prototype.hasOwnProperty.call(obj2, key);
     const isNewKey = hasKeyObj2 && !hasKeyObj1;
@@ -16,30 +11,42 @@ function calculateDiff(obj1, obj2) {
     const isBothExistedKey = hasKeyObj1 && hasKeyObj2;
 
     if (isNewKey) {
-      node.state = 'new';
-      node.value = obj2[key];
-    } else
-    if (isDeletedKey) {
-      node.state = 'deleted';
-      node.value = obj1[key];
-    } else
-    if (isBothExistedKey && _.isEqual(obj1[key], obj2[key])) {
-      node.state = 'equal';
-      node.value = obj1[key];
-    } else {
-      const values = [obj1[key], obj2[key]];
-      const shouldProcessChildren = values.every((v) => v !== null && typeof v === 'object' && !Array.isArray(v));
-      node.state = 'changed';
-      if (shouldProcessChildren) {
-        node.hasChildren = true;
-        node.value = calculateDiff(obj1[key], obj2[key]);
-      } else {
-        node.value = [obj1[key], obj2[key]];
-      }
+      return {
+        ...acc,
+        [key]: {
+          state: 'new',
+          value: obj2[key],
+        },
+      };
     }
+    if (isDeletedKey) {
+      return {
+        ...acc,
+        [key]: {
+          state: 'deleted',
+          value: obj1[key],
+        },
+      };
+    }
+    if (isBothExistedKey && _.isEqual(obj1[key], obj2[key])) {
+      return {
+        ...acc,
+        [key]: {
+          state: 'equal',
+          value: obj1[key],
+        },
+      };
+    }
+    const values = [obj1[key], obj2[key]];
+    const shouldProcessChildren = values.every((v) => v !== null && typeof v === 'object' && !Array.isArray(v));
+
     return {
       ...acc,
-      [key]: node,
+      [key]: {
+        state: 'changed',
+        hasChildren: shouldProcessChildren,
+        value: shouldProcessChildren ? calculateDiff(...values) : values,
+      },
     };
   }, {});
   return ast;
